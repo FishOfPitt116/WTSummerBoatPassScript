7b8aade8185a396bbc81af318c73fb8b29e42e67
Robbie Fishel <robbie.fishel6@gmail.com>
2021-06-16 16:12:21 -0400

    Added some new methods to the spreadsheet file

--- main.py
+++ main.py
@@ -5,4 +5,9 @@ while True:
     currName = input("Type a name for account lookup. Type 0 to exit the program.") # Ask user for name
     if currName == '0':
         sys.exit(0)
-    s.print_account_information(s.get_account_index(currName))
+    actIndex = s.get_account_index(currName)
+    if actIndex <= 1:
+        s.new_account(currName)
+    else:
+        s.print_account_information(s.get_account_index(currName))
+    # s.print_account_information(s.get_account_index(currName))
--- spreadsheet.py
+++ spreadsheet.py
@@ -5,11 +5,33 @@ gc = gspread.service_account() # Authentication works on my laptop only
 sh = gc.open("Test Boat Passes").sheet1 # Calls the spreadsheet named "Test Boat Passes" in drive
 names = sh.col_values(1) # List of all names in our system
 
+def sort_sheet_by_name():
+    sh.sort((1, 'asc'))
+
 def get_account_index(currName):
     return sas.search(names, currName) + 1
-    
+
 def print_account_information(accountIndex):
     try:
         print(sh.row_values(accountIndex))
     except:
         print('Account not found.')
+
+def add_20_pass(accountIndex):
+    currPasses = str(sh.cell(accountIndex, 2).value)
+    if currPasses == "None":
+        currPasses = ""
+    currPasses += "X"
+    sh.update_cell(accountIndex, 2, currPasses)
+
+def add_50_pass(accountIndex):
+    currPasses = str(sh.cell(accountIndex, 3).value)
+    if currPasses == "None":
+        currPasses = ""
+    currPasses += "X"
+    sh.update_cell(accountIndex, 3, currPasses)
+
+def new_account(name):
+    sh.update_cell(len(names)+1, 1, name)
+    print('New account created under name', name)
+    sort_sheet_by_name()
