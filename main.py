2f778879150bf28296b788f5afcc28695b36f3c5
Robbie Fishel <robbie.fishel6@gmail.com>
2021-06-16 16:39:36 -0400

    added line spaces to make command line easier to read OMEGALUL

--- main.py
+++ main.py
@@ -3,10 +3,12 @@ import sys
 
 while True:
     currName = input("Type a name for account lookup. \nType 0 to exit the program. \nType 1 to add an account.") # Ask user for name
+    print()
     if currName == '0':
         sys.exit(0)
     elif currName == '1':
         newName = input("Please type the new account name.")
+        print()
         actIndex = s.get_account_index(newName)
         if s.account_found(actIndex):
             print("Account already found. Printing information...")
